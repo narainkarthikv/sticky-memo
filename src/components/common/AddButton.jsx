0a56eb47131a18bb63f3845710c721eddaae3f30
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-06-22 01:04:15 +0530

    Added tooltips in common add note button

--- src/components/common/AddButton.jsx
+++ src/components/common/AddButton.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import { Fab, Tooltip } from '@mui/material';
+import PropTypes from 'prop-types'; // Import PropTypes
 
-const AddButton = ({ onClick }) => {
-  return (
-    <Fab color="primary" aria-label="add" onClick={onClick}>
+/**
+ * AddButton component provides a floating action button for adding new items.
+ * @param {object} props - The properties passed to the component.
+ * @param {function} props.onClick - The callback function to execute when the button is clicked.
+ * @returns {JSX.Element} A React component representing an add card button.
+ */
+const AddButton = ({ onClick }) => (
+  <Tooltip title='Add note'>
+    <Fab aria-label='add' color='primary' onClick={onClick}>
       <AddIcon />
     </Fab>
-  );
+  </Tooltip>
+);
+
+// PropTypes for validation
+AddButton.propTypes = {
+  onClick: PropTypes.func.isRequired, // Define onClick as a required function
 };
 
 export default AddButton;
